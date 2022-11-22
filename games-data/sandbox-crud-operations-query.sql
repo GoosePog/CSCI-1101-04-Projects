6cd723886faa1d6139f34f082429b6b86d544f2e
Layla Arreola <laylaarreola2022@gmail.com>
2022-11-22 13:22:07 -0600

    added games data project

---
+++ games-data/sandbox-crud-operations-query.sql
@@ -0,0 +1,7 @@
+--INSERT INTO sandbox(int16_value, double_value, string_value) VALUES (1987, 1000001, 'sus????')
+
+--UPDATE sandbox SET double_value = 0.112322 WHERE id = 115;
+
+--DELETE FROM sandbox WHERE id = 78;
+
+SELECT * FROM sandbox;
